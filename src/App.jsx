06781be7956c063885e0c094ac2b7d5198356962
Confidence Madiope <confidencem97@gmail.com>
2023-03-25 12:56:38 +0200

    devTools

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "react-query";
+import {ReactQueryDevtools} from 'react-query/devtools'
 import Home from "./components/Home";
 import Superheroes from "./components/Superheroes";
 import RQSuperheroes from "./components/RQSuperheroes";
@@ -31,6 +32,7 @@ function App() {
           </Routes>
         </Router>
       </div>
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
     </QueryClientProvider>
   );
 }
