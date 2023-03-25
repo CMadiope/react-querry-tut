7e666d678367046baf358b221aa0a38149baedfb
Confidence Madiope <confidencem97@gmail.com>
2023-03-25 12:37:46 +0200

    .

--- src/components/RQSuperheroes.jsx
+++ src/components/RQSuperheroes.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
 
+const fetchSuperheroes = () => {
+return axios.get(" http://localhost:4000/superheroes");
+};
+
 const RQSuperheroes = () => {
-  const { isLoading, data } = useQuery("super-heroes", () => {
-    return axios.get(" http://localhost:4000/superheroes");
-  });
+  const { isLoading, data } = useQuery("super-heroes", fetchSuperheroes);
 
   if (isLoading) {
     return <h3>Loading ...</h3>;
@@ -13,12 +15,10 @@ const RQSuperheroes = () => {
 
   return (
     <>
-      <h2 className="text-2xl py-5">RQSuperheroes</h2>
+      <h2 className='text-2xl py-5'>RQSuperheroes</h2>
       <div>
         {data?.data.map((hero) => (
-          <div key={hero.id}>
-            {hero.name}
-          </div>
+          <div key={hero.id}>{hero.name}</div>
         ))}
       </div>
     </>
